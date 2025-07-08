d8fbaa1594edaebd7d252c547331a69724e0b942
TinashG7 <tinashegatimu@gmail.com>
2025-07-08 14:50:26 +0300

    Firebase confg additions

---
+++ public/firebase_config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC5nqsf7izdWRmEjQlWhxfqgfurVpXZHrE",
+  authDomain: "peer-skill-connect-c4701.firebaseapp.com",
+  databaseURL:
+    "https://peer-skill-connect-c4701-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "peer-skill-connect-c4701",
+  storageBucket: "peer-skill-connect-c4701.firebasestorage.app",
+  messagingSenderId: "392706159751",
+  appId: "1:392706159751:web:724ad728bba4bbe62bb422",
+  measurementId: "G-BL9Q36HHWC",
+};
